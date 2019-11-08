1131b6711cde775e752b4a299b06db3f1e5879c9
David Pink <dvdpink@gmail.com>
2019-11-08 17:18:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay (musicians, instrument) {
+  var musiciansArray = []
+  
+  for (var l = 0, i = musiciansCounted.length; l < i; l++) {
+    musiciansArray.push('musicians[l] =+ plays =+ instrument[l]')
+  }
+  return musiciansArray;
+}
