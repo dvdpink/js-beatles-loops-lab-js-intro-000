4bc62f6f6dee951bda358f4b4de5d8248d38314b
David Pink <dvdpink@gmail.com>
2019-11-08 17:18:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay (musicians, instrument) {
   var musiciansArray = []
-  
+
   for (var l = 0, i = musiciansCounted.length; l < i; l++) {
     musiciansArray.push('musicians[l] =+ plays =+ instrument[l]')
   }
